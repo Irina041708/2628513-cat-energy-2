b191696edfffd437539a6e14232c7de99c131654
Irina041708 <q-w-i-n-86@rambler.ru>
2025-07-09 13:25:33 +0400

    menu2

--- source/scripts/modal.js
+++ source/scripts/modal.js
@@ -2,7 +2,6 @@
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 
-
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', () => {
@@ -14,19 +13,3 @@ navToggle.addEventListener('click', () => {
     navMain.classList.remove('main-nav--opened');
   }
 });
-
-// const blockMap = document.querySelector('.location__link');
-// const staticMap = document.querySelector('.location__link-static');
-// const dynamicMap = document.querySelector('.location__link-dynamic');
-
-// blockMap.classList.remove('location__link--nojs');
-// blockMap.classList.add('location__link--js');
-
-// if (blockMap.classList.contains('location__link--nojs')) {
-//   blockMap.classList.remove(dynamicMap);
-//   blockMap.classList.add(staticMap);
-// } else {
-//   blockMap.classList.add(dynamicMap);
-//   blockMap.classList.remove(staticMap);
-// }
-
